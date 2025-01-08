48f806a1ae19bff3e8e63bf68c7b2b4db397c793
pinnacle-pin <pinnaclepintoken@gmail.com>
2025-01-08 20:07:22 +0300

    Update TokenICO.sol

--- contracts/TokenICO.sol
+++ contracts/TokenICO.sol
@@ -1,3 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
+pragma solidity 0.8.0;
+Interface ERC20 {
+function transfer(address recipient, uint256 amount) external returns(bool):
+function balanceof (address account) external view returns (uint256);
+function allowance(address owner, address spender) external view returns (uint256);
+function approve(address spender, uint256 amount) external returns (bool);
+function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+function symbol() external view returns(string memory);
+function totalSupply() external view returns(uint256);
+function name() external view returns (string memory);
+}
